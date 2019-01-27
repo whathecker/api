01a511b151f3622a4de95d54db1acd50769307b1
simplist88 <yunjae.oh.kr@gmail.com>
2019-01-27 21:52:35 +0100

    add https

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const connector = require('./utils/connector');
-const session = require('express-session');
-const Mongostore = require('connect-mongo')(session);
-const passport = require('passport');
-const morgan = require('morgan');
-const errorhandler = require('errorhandler');
-const apiAuthentication = require('./middlewares/auth');
-const cors = require('cors');
-const dbString = connector.getDBString();
-const app = express();
+const express = require('express'),
+    fs = require('fs'),
+    https = require('https'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    connector = require('./utils/connector'),
+    session = require('express-session'),
+    Mongostore = require('connect-mongo')(session),
+    passport = require('passport'),
+    morgan = require('morgan'),
+    errorhandler = require('errorhandler'),
+    apiAuthentication = require('./middlewares/auth'),
+    cors = require('cors'),
+    dbString = connector.getDBString(),
+    app = express();
 
 
 // connect database
@@ -19,41 +21,47 @@ mongoose.connect(dbString, (err) => {
     console.log(`successfully connected to database`);
 }); 
 
-
 // configure application
-
 const isProduction = process.env.NODE_ENV === "production";
+let sslOptions = {};
 
 if (!isProduction) {
     app.use(errorhandler());
+    // read local SSL key and certificate    
+    sslOptions = {
+        key: fs.readFileSync('./localhost-key.pem'),
+        cert: fs.readFileSync('./localhost-cert.pem')
+    } 
 }
 
-// print error log to stderr stream
+// print morgan log to stderr stream
+
+/*
 app.use(morgan('dev', {
     skip: (req, res) => {
         return res.statusCode < 400
     }, stream: process.stderr
 }));
 
-// print log to stdout stream
+// print morgan log to stdout stream
 app.use(morgan('dev', {
     skip: (req, res) => {
         return res.statusCode >= 400
     }, stream: process.stdin
-}));
+})); */
 
+app.use(morgan('dev'));
 app.use(cors());
 app.use(session({ 
     cookie: {
       maxAge: 12000,
-      secure: false
+      secure: true
     },
     secret: "secret",
     saveUninitialized: false,
     resave: false,
     store: new Mongostore({ url: dbString })
 }));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -95,11 +103,15 @@ app.use((err, req, res, next) => {
 });
 
 
+module.exports = app; // for testing
+
 let port = process.env.PORT || 3001;
 
-// listen to server
-app.listen(port, () => {
-    console.log('app is listening to: ' + port + ` on ${process.env.NODE_ENV}`);
-});
+const server = https.createServer(sslOptions, app);
+server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`https server is running on port number : ${port}`);
+    console.log(`server is running on ${process.env.NODE_ENV}`);
+    console.log(`current log level is set to :  ${process.env.LOG_LEVEL}`);
+}); 
 
-module.exports = app; // for testing
