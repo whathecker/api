79cd378a425486e8e39e4f449707b09650d426a1
Yunjae Oh <yunjae.oh.nl@gmail.com>
2019-02-28 21:57:54 +0100

    update middlewarew for debugging

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -2,16 +2,18 @@
 function isAuthenticated (req, res, next) {
 
     console.log(req.session);
+
+    /*
     console.log(req.session.cookie);
     console.log(req.session.passport);
     console.log(req.user);
-
+    
     if (!req.session.passport.user) {
         console.log('unauthorized user');
         res.status(401).json({ message: 'unauthoized user'});
     } else {
         next();
-    }
+    } */
 }
 
 module.exports = isAuthenticated;
