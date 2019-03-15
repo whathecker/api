4387193dfedde6773750af56c68674cd7ffc533e
Yunjae Oh <yunjae.oh.nl@gmail.com>
2019-03-15 01:18:07 +0100

    change error message for 401 when api key isn't in place

--- app/middlewares/verifyApikey.js
+++ app/middlewares/verifyApikey.js
@@ -4,12 +4,12 @@ function verifyApikey (req, res, next) {
     //console.log(req.get('apikey'));
 
     if (!req.get('X-API-Key')) {
-        res.status(401).json({ message: 'Request is denied' });
+        res.status(401).json({ message: 'unauthroized' });
     } else if (req.get('X-API-Key')) {
 
         Apikey.findOne({ key: req.get('X-API-Key') }).then((key) => {
             if (!key) {
-                res.status(401).json({ message: 'Request is denied' });
+                res.status(401).json({ message: 'unauthroized' });
             } else {
                 next();
             }
