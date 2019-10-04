d1bf1776c88a159efe6c5437ba7a266c3ac4cb00
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2019-10-04 14:30:03 +0200

    add getUserSubscription endpoint

--- app/controllers/user/getUserSubscription.js
+++ app/controllers/user/getUserSubscription.js
@@ -10,9 +10,43 @@ async function getUserSubscription (req, res, next) {
         populate: { path: 'paymentMethod'}
     })
     .then(user => {
-        console.log(user);
-        console.log(user.subscriptions[0])
-        return res.status(200).end();
+
+        if (!user) {
+            logger.warn(`getUserSubscription request is failed | unknown user`);
+            return res.status(422).json({
+                status: 'failed',
+                message: 'unknown user'
+            });
+        }
+
+        if (user && !user.subscriptions[0]) {
+            logger.warn(`getUserSubscription request is failed | no subscription`);
+            return res.status(422).json({
+                status: 'failed',
+                message: 'no subscription'
+            });
+        }
+
+        if (user && user.subscriptions[0]) {
+
+            const subscription = {
+                channel: user.subscriptions[0].channel,
+                deliveryFrequency: user.subscriptions[0].deliveryFrequency,
+                deliveryDay: user.subscriptions[0].deliveryDay,
+                isActive: user.subscriptions[0].isActive,
+                creationDate: user.subscriptions[0].creationDate,
+                lastModified: user.subscriptions[0].lastModified,
+                deliverySchedules: user.subscriptions[0].deliverySchedules,
+                subscriptionId: user.subscriptions[0].subscriptionId
+            }
+            logger.info(`getUserSubscription request is processed | ${subscription.subscriptionId} has returned`)
+            return res.status(200).json({
+                status: 'success',
+                subscription: subscription,
+                message: 'subscription is returned'
+            });
+        }
+        
     }).catch(next);
 }
 
