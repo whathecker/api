eef20196bf53ff0d48e96475203da4132b6f68d8
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-04-05 21:26:15 +0200

    add unit tests and implementaiton till validateOrderStatus

--- src/domain/order/factory/index.js
+++ src/domain/order/factory/index.js
@@ -10,6 +10,15 @@ const enum_country_prefixes = Object.freeze({
     netherlands: "NL",
 });
 
+const enum_order_status = Object.freeze({
+    0: "RECEIVED",
+    1: "PENDING",
+    2: "PAID",
+    3: "SHIPPED",
+    4: "CANCELLED",
+    5: "OVERDUE"
+});
+
 class OrderFacotry {
     constructor({
 
@@ -86,19 +95,69 @@ class OrderFacotry {
         return result;
     }
 
-    static createOrderNumber () {
+    static createOrderNumber ({
+        envPrefix,
+        countryPrefix
+    } = {}) {
 
+        (envPrefix === null)? envPrefix = "DV" : null;
+        (countryPrefix === null)? countryPrefix =  "NL" : null;
+
+        const fiveDigitsNum = this.create_five_digits_integer();
+
+        return ''.concat(envPrefix, countryPrefix,fiveDigitsNum);
     }
 
-    static validateInvoiceNumber () {
-
+    static create_five_digits_integer () {
+        const num = Math.floor(Math.random() * 90000) + 10000;
+        return num.toString();
     }
 
-    static validateOrderStatus () {
-
+    static get_env_prefix (envVar) {
+        const envPrefix = enum_env_prefixes[envVar];
+        
+        if (!envPrefix) {
+            return null;
+        }
+        if (envPrefix) {
+            return envPrefix;
+        }
     }
 
-    static validateOrderStatusHistory () {
+    static get_country_prefix (country) {
+        const countryPrefix = enum_country_prefixes[country];
+
+        if (!countryPrefix) {
+            return null;
+        }
+        if (countryPrefix) {
+            return countryPrefix;
+        }
+    }
+
+    static validateInvoiceNumber (invoiceNumber) {
+        if (invoiceNumber.length === 13) {
+            return true;
+        }
+        return false;
+    }
+
+    static validateOrderStatus ({
+        status,
+        timestamp
+    } = {}) {
+        let result = false;
+
+        for (let prop of Object.keys(enum_order_status)) {
+            if (status === enum_order_status[prop]) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    static validateOrderStatusHistory (orderStatusHistory) {
 
     }
 
--- src/domain/order/factory/index.unit.test.js
+++ src/domain/order/factory/index.unit.test.js
@@ -73,4 +73,145 @@ describe('Test OrderFactory', () => {
       
         expect(result).toBe(false);
     });
+
+    test('get_env_prefix must return null', () => {
+        const envVar = "new";
+
+        const envPrefix = OrderFactory.get_env_prefix(envVar);
+
+        expect(envPrefix).toBe(null);
+    });
+
+    test('get_env_prefix must return correct prefix', () => {
+        const envPrefix = OrderFactory.get_env_prefix("test");
+        const envPrefix2 = OrderFactory.get_env_prefix("local");
+        const envPrefix3 = OrderFactory.get_env_prefix("development");
+        const envPrefix4 = OrderFactory.get_env_prefix("staging");
+        const envPrefix5 = OrderFactory.get_env_prefix("production");
+        
+        expect(envPrefix).toBe("DV");
+        expect(envPrefix2).toBe("DV");
+        expect(envPrefix3).toBe("DV");
+        expect(envPrefix4).toBe("ST");
+        expect(envPrefix5).toBe("EC");
+    });
+
+    test('get_country_prefix must return undefined', () => {
+        const country = "new";
+
+        const countryPrefix = OrderFactory.get_country_prefix(country);
+
+        expect(countryPrefix).toBe(null);
+    });
+
+    test('get_country_prefix must return correct prefix', () => {
+        const country = "netherlands";
+
+        const countryPrefix = OrderFactory.get_country_prefix(country);
+
+        expect(countryPrefix).toBe("NL");
+    });
+
+    test('createOrderNumber must return valid orderNumber', () => {
+        const envPrefix = OrderFactory.get_env_prefix('production');
+        const countryPrefix = OrderFactory.get_country_prefix('netherlands');
+        const orderNumber = OrderFactory.createOrderNumber({
+            envPrefix: envPrefix,
+            countryPrefix: countryPrefix
+        });
+
+        const envPrefix2 = OrderFactory.get_env_prefix('staging');
+        const countryPrefix2 = OrderFactory.get_country_prefix('netherlands');
+        const orderNumber2 = OrderFactory.createOrderNumber({
+            envPrefix: envPrefix2,
+            countryPrefix: countryPrefix2
+        });
+
+        const envPrefix3 = OrderFactory.get_env_prefix('new');
+        const countryPrefix3 = OrderFactory.get_country_prefix('new');
+        const orderNumber3 = OrderFactory.createOrderNumber({
+            envPrefix: envPrefix3,
+            countryPrefix: countryPrefix3
+        });
+
+        expect(orderNumber).toHaveLength(9);
+        expect(orderNumber.slice(0,2)).toBe('EC');
+        expect(orderNumber.slice(2,4)).toBe('NL');
+
+        expect(orderNumber2).toHaveLength(9);
+        expect(orderNumber2.slice(0,2)).toBe('ST');
+        expect(orderNumber2.slice(2,4)).toBe('NL');
+
+        expect(orderNumber3).toHaveLength(9);
+        expect(orderNumber3.slice(0,2)).toBe('DV');
+        expect(orderNumber3.slice(2,4)).toBe('NL');
+    });
+
+    test('validateInvoiceNumber must return true', () => {
+        const invoiceNumber = "0805081926622";
+
+        const result = OrderFactory.validateInvoiceNumber(invoiceNumber);
+
+        expect(result).toBe(true);
+    });
+
+    test('validateInvoiceNumber must return false', () => {
+        const invoiceNumber = "080508192662211";
+
+        const result = OrderFactory.validateInvoiceNumber(invoiceNumber);
+
+        expect(result).toBe(false);
+    });
+
+    test('validateOrderStatus must return true', () => {
+        const orderStatus = {
+            status: 'RECEIVED',
+            timestamp: new Date('December 17, 1995 03:24:00')
+        };
+        const orderStatus2 = {
+            status: 'PENDING',
+            timestamp: new Date('December 18, 1995 03:24:00')
+        };
+        const orderStatus3 = {
+            status: 'PAID',
+            timestamp: new Date('December 19, 1995 03:24:00')
+        };
+        const orderStatus4 = {
+            status: 'SHIPPED',
+            timestamp: new Date('December 20, 1995 03:24:00')
+        };
+        const orderStatus5 = {
+            status: 'CANCELLED',
+            timestamp: new Date('December 21, 1995 03:24:00')
+        };
+        const orderStatus6 = {
+            status: 'OVERDUE',
+            timestamp: new Date('December 22, 1995 03:24:00')
+        };
+
+        const result = OrderFactory.validateOrderStatus(orderStatus);
+        const result2 = OrderFactory.validateOrderStatus(orderStatus2);
+        const result3 = OrderFactory.validateOrderStatus(orderStatus3);
+        const result4 = OrderFactory.validateOrderStatus(orderStatus4);
+        const result5 = OrderFactory.validateOrderStatus(orderStatus5);
+        const result6 = OrderFactory.validateOrderStatus(orderStatus6);
+        
+        expect(result).toBe(true);
+        expect(result2).toBe(true);
+        expect(result3).toBe(true);
+        expect(result4).toBe(true);
+        expect(result5).toBe(true);
+        expect(result6).toBe(true);
+    });
+
+    test('validateOrderStatus must return true', () => {
+        const orderStatus = {
+            status: 'TOBEPAID',
+            timestamp: new Date('December 17, 1995 03:24:00')
+        };
+        
+        const result = OrderFactory.validateOrderStatus(orderStatus);
+        
+        expect(result).toBe(false);
+    });
 });
