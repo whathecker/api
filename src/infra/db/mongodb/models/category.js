77204502d9878350764e8e70acc959a141597c41
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-04-16 22:37:53 +0200

    add mongodb model for category object

---
+++ src/infra/db/mongodb/models/category.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+let categorySchema = new Schema({
+    categoryName: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    categoryCode: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        uppercase: true 
+    }
+});
+
+categorySchema.plugin(uniqueValidator);
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
