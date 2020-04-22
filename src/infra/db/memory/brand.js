6b0350837a1c6c7e88fe1af5198d3e2e6d99fabe
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-04-22 20:15:44 +0200

    add in memory data for brand object

---
+++ src/infra/db/memory/brand.js
@@ -0,0 +1,35 @@
+const brands = [
+    {
+        _id: "1",
+        brandCode: "LG",
+        brandName: "Leegeeham"
+    },
+    {
+        _id: "2",
+        brandCode: "MS",
+        brandName: "Missha"
+    },
+    {
+        _id: "3",
+        brandCode: "KL",
+        brandName: "Klaris"
+    },
+    {
+        _id: "4",
+        brandCode: "AR",
+        brandName: "Aromatica"
+    },
+    {
+        _id: "5",
+        brandCode: "HH",
+        brandName: "Holika Holika"
+    },
+    {
+        _id: "6",
+        brandCode: "BT",
+        brandName: "Benton"
+    },
+
+];
+
+module.exports = brands;
