6099d49f954fcfa7dbc8696b5c3f3a133dad23f0
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-04-23 21:38:40 +0200

    implement in memory db access layer for skinType object

--- src/infra/data-access/skinType-db/index.unit.test.js
+++ src/infra/data-access/skinType-db/index.unit.test.js
@@ -20,25 +20,19 @@ describe('Test database access layer of skinType object', () => {
                 skinType: "normal",
                 skinTypeCode: "NM"
             },
-            {
-                skinType: "oily",
-                skinTypeCode: "OL"
-            }
         ];
 
         const skinType = await skinTypeDB.addSkinType(mockSkinTypes[0]);
         const skinType2 = await skinTypeDB.addSkinType(mockSkinTypes[1]);
-        const skinType3 = await skinTypeDB.addSkinType(mockSkinTypes[2]);
 
         _skinType_id_holder[0] = skinType._id;
         _skinType_id_holder[1] = skinType2._id;
-        _skinType_id_holder[2] = skinType3._id;
     });
 
     test('list all skin types', async () => {
         const skinTypes = await skinTypeDB.listSkinTypes();
 
-        expect(skinTypes).toHaveLength(3);
+        expect(skinTypes).toHaveLength(2);
     });
 
     test('find skin type by name', async () => {
@@ -57,8 +51,8 @@ describe('Test database access layer of skinType object', () => {
 
     test('add a new skin type', async () => {
         const payload = {
-            skinType: "dry",
-            skinTypeCode: "DR"
+            skinType: "oily",
+            skinTypeCode: "OL"
         };
 
         const newSkinType = await skinTypeDB.addSkinType(payload);
@@ -76,7 +70,7 @@ describe('Test database access layer of skinType object', () => {
 
         expect(result.status).toBe('success');
         expect(result._id).toEqual(skinType_id);
-        expect(skinTypes).toHaveLength(2);
+        expect(skinTypes).toHaveLength(1);
     });
 
     test('drop all skin types in db', async () => {
--- src/infra/data-access/skinType-db/memory/index.js
+++ src/infra/data-access/skinType-db/memory/index.js
@@ -1,24 +1,118 @@
-const SKINTYPES = require('../../../db/memory/skinType');
+let SKINTYPES = require('../../../db/memory/skinType');
 const createSkinTypeObj = require('../../../../domain/skinType');
 
 const listSkinTypes = () => {
-
+    return Promise.resolve(SKINTYPES);
 };
 
-const findSkinTypeByName = () => {
+const findSkinTypeByName = (skinTypeName) => {
+    const skinType = SKINTYPES.find(skinType => {
+        // TODO: see how confusing below skinType.skinType is
+        // please rename it 
+        return skinType.skinType === skinTypeName;
+    });
 
+    if (!skinType) {
+        return Promise.reject({
+            status: "fail",
+            reason: "skinType not found"
+        });
+    }
+
+    return Promise.resolve(skinType);
 };
 
-const addSkinType = () => {
+const addSkinType = (payload) => {
 
+    const skinType = createSkinTypeObj(payload);
+
+    if (skinType instanceof Error) {
+        return Promise.reject({
+            status: "fail",
+            reason: "error",
+            error: skinType
+        });
+    }
+
+    try {
+        _isSkinTypeNameUnique(skinType.skinType);
+        _isSkinTypeCodeUnique(skinType.skinTypeCode);
+    }
+    catch (err) {
+        return Promise.reject({
+            status: "fail",
+            reason: "error",
+            error: err
+        });
+    }
+
+    const new_id = SKINTYPES.length + 1;
+
+    const newSkinType = {
+        _id: new_id.toString(),
+        skinType: skinType.skinType,
+        skinTypeCode: skinType.skinTypeCode
+    };
+    SKINTYPES.push(newSkinType);
+
+    return Promise.resolve(SKINTYPES[SKINTYPES.length - 1]);
 };
 
-const deleteSkinTypeByName = () => {
+async function _isSkinTypeNameUnique (skinTypeName) {
+    const skinType = await findSkinTypeByName(skinTypeName);
 
+    const { status } = skinType;
+
+    if (status === "fail") return;
+
+    throw new Error("db access for skinType object failed: skinType must be unique");
+}
+
+function _isSkinTypeCodeUnique (skinTypeCode) {
+    const skinType = SKINTYPES.find(skinType => {
+        return skinType.skinTypeCode === skinTypeCode;
+    });
+
+    if (!skinType) return;
+
+    throw new Error("db access for skinType object failed: skinTypeCode must be unique");
+}
+
+const deleteSkinTypeByName = async (skinTypeName) => {
+
+    const skinType = await findSkinTypeByName(skinTypeName);
+
+    const { status } = skinType;
+
+    if (status === "fail") {
+        return Promise.reject({
+            status: "fail",
+            reason: "skinType not found"
+        });
+    }
+
+    let deleteSkinType;
+    SKINTYPES = SKINTYPES.filter(skinType => {
+
+        if (skinType.skinType !== skinTypeName) {
+            return true;
+        }
+
+        if (skinType.skinType === skinTypeName) {
+            deleteSkinType = skinType;
+            return false;
+        }
+    });
+
+    return Promise.resolve({
+        _id: deleteSkinType._id,
+        status: "success"
+    });
 };
 
 const dropAll = () => {
-
+    SKINTYPES = [];
+    return Promise.resolve(SKINTYPES);
 };
 
 module.exports = {
