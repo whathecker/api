8c17ec4f9bf68d52a6602ea26429363c12e3e01f
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-04-27 20:19:40 +0900

    add failing unit tests for product db access layer

---
+++ src/infra/data-access/product-db/test/product-db.unit.test.js
@@ -0,0 +1,111 @@
+const productDB = require('../index');
+let mockProducts = require('./mock_products');
+
+describe('Test database layer of product object', () => {
+
+    const _productId_holder = {
+        0: null,
+        1: null,
+        2: null
+    };
+
+    beforeEach(async () => {
+        await productDB.dropAll();
+
+        const product = await productDB.addProduct(mockProducts[0]);
+        const product2 = await productDB.addProduct(mockProducts[1]);
+        const product3 = await productDB.addProduct(mockProducts[2]);
+
+        _productId_holder[0] = product.productId;
+        _productId_holder[1] = product2.productId;
+        _productId_holder[2] = product3.productId;
+    });
+
+    test('list all products', async () => {
+        const products = await productDB.listProducts();
+
+        expect(products).toHaveLength(3);
+    });
+
+    test('find product by productId', async () => {
+        const productId = mockProducts[0].productId;
+
+        const product = await productDB.findProductByProductId(productId);
+        const {_id, ...rest} = product;
+
+        expect(rest).toEqual(mockProducts[0]);
+    });
+
+    test('add a new product', async () => {
+        
+        const payload = {
+            channel: "EU",
+            name: "Other Pure Essence Mask Sheet-Avocado",
+            description: "This sheetmask is good for something",
+            category: "Sheetmask",
+            categoryCode: "ST",
+            brand: "Holika Holika",
+            brandCode: "HH",
+            skinType: "dry",
+            inventory: {
+                quantityOnHand: 10,
+                quarantaine: 0,
+                lastModified: new Date('December 19, 2000 03:24:00')
+            },
+            inventoryHistory: [
+                {
+                    quantityOnHand: 0,
+                    quarantaine: 0,
+                    lastModified: new Date('December 19, 1999 03:24:00')
+                },
+                {
+                    quantityOnHand: 10,
+                    quarantaine: 0,
+                    lastModified: new Date('December 19, 2000 03:24:00')
+                },
+            ],
+            prices: [
+                {
+                    region: "eu",
+                    currency: "euro",
+                    price: "2.95",
+                    vat: "0.51",
+                    netPrice: "2.44"
+                }
+            ],
+            creationDate: new Date('December 19, 1996 03:24:00'),
+            lastModified: new Date('December 19, 2000 03:24:00')
+        };
+
+        const newProduct = await productDB.addProduct(payload);
+        const {_id, productId, ...rest } = newProduct;
+
+        expect(rest).toEqual(payload);
+    });
+
+    /*
+    TODO: implement later
+    Need requirements on how to allow user to update the field
+    test('update a product', () => {
+
+    }); */
+
+    test('delete a product by productId', async () => {
+        const productId = mockProducts[1].productId;
+
+        const result = await productDB.deleteProductByProductId();
+        const products = await productDB.listProducts();
+        const {status, productId} = result;
+
+        expect(status).toBe('success');
+        expect(productId).toEqual(productId);
+        expect(products).toHaveLength(2);
+    });
+
+    test('drop all products in db', async () => {
+        await productDB.dropAll();
+        const products = await productDB.listProducts();
+
+        expect(products).toHaveLength(0);
+    });
+});
