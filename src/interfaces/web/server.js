b2304afc37a43849c6ed8e214e58bd43dff95a65
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-05-07 20:11:04 +0900

    add scaffolding for script to start web server

---
+++ src/interfaces/web/server.js
@@ -0,0 +1,9 @@
+const app = require('./starter');
+
+app.listen(process.env.PORT, (err) => {
+    if (err) throw err;
+    console.log(`server is running on port number: ${process.env.PORT}`);
+    console.log(`current env is : ${process.env.NODE_ENV}`);
+    console.log(`current log level is : ${process.env.LOG_LEVEL}`);
+});
+
