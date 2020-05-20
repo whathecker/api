c06fbae64d75070537978b1a86ff8c5150cc44af
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-05-20 19:37:40 +0900

    implement createCategory endpoint

--- src/interfaces/web/starter/routes/api/category/index.js
+++ src/interfaces/web/starter/routes/api/category/index.js
@@ -18,8 +18,34 @@ category.listCategories = async (req, res, next) => {
     }
 };
 
-category.createCategory = (req, res, next) => {
-    return res.status(200).end();
+category.createCategory = async (req, res, next) => {
+    const categoryName = req.body.categoryName;
+    const categoryCode = req.body.categoryCode;
+
+    if (!categoryName || ! categoryCode) {
+        logger.error(`createCategory request has failed | missing parameter`);
+        return res.status(400).json({
+            status: 'failed',
+            message: 'bad request'
+        });
+    }
+
+    try {
+        const payload = {
+            categoryName: req.body.categoryName,
+            categoryCode: req.body.categoryCode
+        };
+
+        const category = await categoryDB.addCategory(payload);
+        logger.info(`createCategory request has created new category | name: ${category.categoryName} code: ${category.categoryCode}`);
+        return res.status(201).json({
+            status: 'success',
+            message: 'new category created'
+        });
+
+    } catch (err) {
+        next(err);
+    }
 };
 
 module.exports = category;
