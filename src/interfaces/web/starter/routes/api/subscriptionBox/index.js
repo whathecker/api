241ca0bdfe1b537fd544bd955267587f65473201
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-05-27 22:05:39 +0900

    implement createSubscriptionBox endpoint

--- src/interfaces/web/starter/routes/api/subscriptionBox/index.js
+++ src/interfaces/web/starter/routes/api/subscriptionBox/index.js
@@ -21,7 +21,42 @@ subscriptionBox.getSubscriptionBoxById = async (req, res, next) => {
 };
 
 subscriptionBox.createSubscriptionBox = async (req, res, next) => {
-    return res.status(200).end();
+    const channel = req.body.channel;
+    const name = req.body.name;
+    const boxType = req.body.boxType;
+    const boxTypeCode = req.body.boxTypeCode;
+    const prices = req.body.prices;
+
+    if (!channel||!name||!boxType||!boxTypeCode||!prices) {
+        logger.warn(`createSubscriptionBox request has rejected as param is missing`);
+        return res.status(400).json({ 
+            status: 'failed',
+            message: 'bad request' 
+        });
+    }
+
+    try {
+        const payload = req.body;
+        const subscriptionBox = await packageDB.addSubscriptionBox(payload);
+        logger.info(`createSubscriptionBox request has created new product | name: ${subscriptionBox.name} productId: ${subscriptionBox.packageId}`);
+        return res.status(201).json({
+            status: 'success',
+            subscriptionBox: subscriptionBox,
+            message: 'new subscriptionBox created'
+        });
+
+    } catch (exception) {
+        if (exception.status === "fail") {
+            logger.error(`createSubscriptionBox request has failed | error: ${exception.error.message}`);
+            return res.status(422).json({
+                status: "fail",
+                message: exception.error.message
+            });
+        } else {
+            next(exception);
+        }
+    }
+
 };
 
 subscriptionBox.updateSubscriptionBox = async (req, res, next) => {
--- src/interfaces/web/starter/routes/api/subscriptionBox/test/subscriptionBox.int.test.js
+++ src/interfaces/web/starter/routes/api/subscriptionBox/test/subscriptionBox.int.test.js
@@ -4,6 +4,23 @@ const session = require('supertest-session');
 serverStarter.loadMiddlewares();
 let testSession = session(serverStarter.app);
 
+let payload = {
+    name: "Package 1",
+    channel: "EU",
+    items: ["1", "2"],
+    prices: [{
+        region: "eu",
+        currency: "euro",
+        price: "19.00",
+        vat: "3.30",
+        netPrice: "15.70"
+    }],
+    boxType: "dry",
+    boxTypeCode: "DR",
+    creationDate: new Date('December 17, 1995 03:24:00'),
+    lastModified: new Date('December 17, 1999 03:24:00') 
+};
+
 describe('Test subscriptionBoxes endpoint', () => {
     
     afterEach(async () => {
@@ -19,5 +36,45 @@ describe('Test subscriptionBoxes endpoint', () => {
         .then(response => {
             expect(response.status).toBe(200);
         });
+    });
+
+    // test for getSubscriptionBoxById
+
+    test('createSubscriptionBox fail - bad request', () => {
+        return testSession.post('/subscriptionBoxes/subscriptionBox')
+        .send({})
+        .then(response => {
+            expect(response.status).toBe(400);
+        });
+    });
+
+    test('createSubscriptionBox success', () => {
+        return testSession.post('/subscriptionBoxes/subscriptionBox')
+        .send(payload)
+        .then(response => {
+            expect(response.status).toBe(201);
+        });
+    });
+
+    test('createSubscriptionBox fail - invalid payload', () => {
+        let deepCopiedPayload = JSON.parse(JSON.stringify(payload));
+        deepCopiedPayload.channel = "ODD CHANNEL";
+        return testSession.post('/subscriptionBoxes/subscriptionBox')
+        .send(deepCopiedPayload)
+        .then(response => {
+            expect(response.status).toBe(422);
+        }); 
+    });
+    
+    test('createSubscriptionBox fail - duplicated packageId', async () => {
+        const response = await testSession.post('/subscriptionBoxes/subscriptionBox').send(payload);
+        const packageId = response.body.subscriptionBox.packageId;
+        let deepCopiedPayload = JSON.parse(JSON.stringify(payload));
+        deepCopiedPayload.packageId = packageId;
+        return testSession.post('/subscriptionBoxes/subscriptionBox')
+        .send(deepCopiedPayload)
+        .then(response => {
+            expect(response.status).toBe(422);
+        }); 
     });
 });
